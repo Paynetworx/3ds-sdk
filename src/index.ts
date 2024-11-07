1dea95bb389b3133ccd30f37dc029f6be7646786
John M Calhoun <john@paynetworx.com>
2024-11-06 23:33:33 -0600

    add browser information gathering

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
-export function greet(name: string): string {
-  return `Hello, ${name}!`;
+export const CHALLENGE_WINDOW_SIZE_250_400 = "01"
+export const CHALLENGE_WINDOW_SIZE_390_400 = "02"
+export const CHALLENGE_WINDOW_SIZE_500_600 = "03"
+export const CHALLENGE_WINDOW_SIZE_600_400 = "04"
+export const CHALLENGE_WINDOW_SIZE_Full_screen = "05"
+
+export function GatherBrowserData(window_size: string) {
+  return {
+    browserJavaEnabled: navigator.javaEnabled,
+    browserLanguage: navigator.language,
+    browserColorDepth: screen.colorDepth,
+    browserScreenHeight: screen.height,
+    browserScreenWidth: screen.width,
+    browserTZ:  (new Date()).getTimezoneOffset(),
+    challengeWindowSize:  window_size
+  }
 }
