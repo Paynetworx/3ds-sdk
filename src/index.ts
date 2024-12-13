e990eee771393fccc7e7215db803824170e75216
John M Calhoun <john@paynetworx.com>
2024-12-13 16:14:42 -0600

    small updates

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,13 @@ export const CHALLENGE_WINDOW_SIZE_500_600 = "03"
 export const CHALLENGE_WINDOW_SIZE_600_400 = "04"
 export const CHALLENGE_WINDOW_SIZE_Full_screen = "05"
 
-export function GatherBrowserData(window_size: string) {
+export async function GatherBrowserData(window_size: string, echo_url: string) {
+
+  const response = await fetch(echo_url)
+  const data = await response.json() as {
+    id: string
+  }
+
   return {
     browserJavaEnabled: navigator.javaEnabled,
     browserLanguage: navigator.language,
@@ -12,6 +18,7 @@ export function GatherBrowserData(window_size: string) {
     browserScreenHeight: screen.height.toString(),
     browserScreenWidth: screen.width.toString(),
     browserTZ:  (new Date()).getTimezoneOffset().toString(),
-    challengeWindowSize:  window_size.toString()
+    challengeWindowSize:  window_size.toString(),
+    browser_info_id: data.id
   }
 }
